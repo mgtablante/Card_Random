a5324338673082f5b93d5aba8522233faf4691ba
mgtablante <mgtablantel@gmail.com>
2021-07-14 19:43:02 +0000

    final delivery

--- src/app.js
+++ src/app.js
@@ -2,10 +2,22 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function main() {
+  document.getElementById("btn").addEventListener("click", changeCard);
+}
+function changeCard() {
+  let poker = ["heart", "spades", "blackclub", "diamond"];
+  let card = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+  let numberRandom = Math.floor(Math.random() * card.length);
+  console.log(card[numberRandom]);
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let elementCard = document.querySelector(".myCard");
+  let elementNumber = document.querySelector(".theNumber");
+  elementNumber.innerHTML = card[numberRandom];
+
+  let randomPoker = poker[Math.floor(Math.random() * poker.length)];
+  elementCard.className = "myCard";
+  elementCard.classList.add(randomPoker);
+}
+
+window.addEventListener("load", () => main());
